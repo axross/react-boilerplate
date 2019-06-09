8b922912179065c038664c202221c3426aa00a20
Kohei Asai <me@axross.io>
2019-06-08 23:16:49 -0700

    Use NavigationBar in SignedInScene

--- src/components/scenes/SignedInScene/SignedInScene.tsx
+++ src/components/scenes/SignedInScene/SignedInScene.tsx
@@ -1,22 +1,23 @@
 import styled from "@emotion/styled";
 import { Router } from "@reach/router";
 import * as React from "react";
-import DocumentTitle from "./common/DocumentTitle";
-import SignOutButton from "./common/SignOutButton";
-import TaskListRoute from "./routes/TaskListRoute";
+import DocumentTitle from "../../common/DocumentTitle";
+import NavigationBar from "../../common/NavigationBar";
+import SignOutButton from "../../common/SignOutButton";
+import TaskListRoute from "../../routes/TaskListRoute";
 
 function SignedInScene(): React.ReactElement {
   return (
     <>
-      <DocumentTitle title="React Boilerplate" />
+      <DocumentTitle title="Clean React" />
+
+      <NavigationBar title="Clean React" actions={<SignOutButton />} />
 
       <React.Suspense fallback={<span>loading...</span>}>
         <Root>
           <Router>
             <TaskListRoute path="/" />
           </Router>
-
-          <SignOutButton />
         </Root>
       </React.Suspense>
     </>
